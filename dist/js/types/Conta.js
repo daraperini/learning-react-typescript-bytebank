233ded2812ea33239585b1530d75a4829b61a7f3
Dara Lahm Perini <daraperini@gmail.com>
2024-03-08 14:57:09 -0300

    creating transactions' summary js

--- dist/js/types/Conta.js
+++ dist/js/types/Conta.js
@@ -48,6 +48,27 @@ const Conta = {
         });
         return gruposTransacoes;
     },
+    agruparTransacoes() {
+        const resumo = {
+            totalDepositos: 0,
+            totalTransferencias: 0,
+            totalPagamentosBoleto: 0,
+        };
+        this.transacoes.forEach((transacao) => {
+            switch (transacao.tipoTransacao) {
+                case TipoTransacao.DEPOSITO:
+                    resumo.totalDepositos += transacao.valor;
+                    break;
+                case TipoTransacao.TRANSFERENCIA:
+                    resumo.totalTransferencias += transacao.valor;
+                    break;
+                case TipoTransacao.PAGAMENTO_BOLETO:
+                    resumo.totalPagamentosBoleto += transacao.valor;
+                    break;
+            }
+        });
+        return resumo;
+    },
     registrarTransacao(novaTransacao) {
         if (novaTransacao.tipoTransacao === TipoTransacao.DEPOSITO) {
             depositar(novaTransacao.valor);
